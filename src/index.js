942d55d4906ac5c8888748e09b3128fe67dbf24a
tjr226 <tjr226@gmail.com>
2019-05-15 16:44:11 -0400

    added depenencies

--- src/index.js
+++ src/index.js
@@ -7,13 +7,16 @@ import { createStore } from "redux";
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
+import { applyMiddleware } from redux;
 // thunk from redux-thunk
+import thunk from redux-thunk;
 // logger from redux-logger
+import logger from redux-logger;
 // rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -5,6 +5,12 @@ const initialState = {
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+      case FETCHING:
+        return;
+      case SUCCESS:
+        return;
+      case FAILURE:
+        return;
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
